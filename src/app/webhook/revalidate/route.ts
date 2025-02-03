cfcf6cb45be3a6f29f553d3550b6a3f0ba469b3d
Jonas Wanner <jonas.wanner@clavisit.com>
2025-02-03 14:39:37 +0100

    adding post for revalidate

--- src/app/webhook/revalidate/route.ts
+++ src/app/webhook/revalidate/route.ts
@@ -1,8 +1,16 @@
 import {revalidatePath} from 'next/cache'
+import {NextRequest} from 'next/server'
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
+  revalidatePath('/webhook')
+  return Response.redirect(`https://notion-next.wanner.work/webhook`)
+}
+
+export async function POST(request: NextRequest) {
   revalidatePath('/webhook')
   return Response.json({
-    done: true
+    revalidated: true,
+    now: Date.now(),
+    message: 'successfully revalidated page'
   })
 }
